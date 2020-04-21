cec18aaf8cd9b834afc7647d18fdcd2eadb05ae2
Léonard <leonard@bigsool.com>
2020-04-21 20:55:33 +0200

    Updates

--- atoms/menu.d.ts
+++ atoms/menu.d.ts
@@ -1,12 +1,9 @@
-import { CSSResult, LitElement } from 'lit-element';
+import { LitElement } from 'lit-element';
 export declare class MenuElement extends LitElement {
-    static readonly is: string;
     items: any[];
     route: any;
     shown: boolean;
-    connectedCallback(): void;
-    disconnectedCallback(): void;
-    static get styles(): CSSResult;
+    createRenderRoot(): this;
     render(): import("lit-element").TemplateResult;
     private _link;
 }

2df1e3c252a12989052ccc2c9e973938c9257895
Léonard <leonard@bigsool.com>
2020-04-22 01:03:45 +0200

    Updates

--- atoms/spinner.d.ts
+++ atoms/spinner.d.ts
@@ -1,6 +1,7 @@
 import { LitElement } from 'lit-element';
 export declare class ElaraSpinner extends LitElement {
     active: boolean;
+    text: string;
     static get styles(): import("lit-element").CSSResult;
     render(): import("lit-element").TemplateResult;
 }

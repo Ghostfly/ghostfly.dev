47037cb12d31ed8053e1baed80fd508e14291b73
Léonard <leonard@bigsool.com>
2020-04-22 02:05:02 +0200

    Updates

--- main.536bdffd.js
+++ main.536bdffd.js
@@ -419,7 +419,7 @@ class r{constructor(e,t,n){this.__parts=[],this.template=e,this.processor=t,this
         `}get picture(){return this.querySelector("#pic")}get container(){return this.querySelector("#container")}};y([Object(i.e)({type:String,reflect:!0})],b.prototype,"route",void 0),y([Object(i.e)({type:Boolean,reflect:!0})],b.prototype,"loaded",void 0),b=y([Object(i.c)("ui-profile")],b);var _=function(e,t,n,i){var s,r=arguments.length,o=r<3?t:null===i?i=Object.getOwnPropertyDescriptor(t,n):i;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)o=Reflect.decorate(e,t,n,i);else for(var a=e.length-1;a>=0;a--)(s=e[a])&&(o=(r<3?s(o):r>3?s(t,n,o):s(t,n))||o);return r>3&&o&&Object.defineProperty(t,n,o),o};let x=class extends f{get head(){return{title:"Work",description:"",type:"page",image:"",slug:"#!home"}}render(){return i.d`
         <div class="home">
             <a rel="noopener" title="Open-source" target="_blank" href="https://github.com/Ghostfly">
-            <svg version="1.1" viewBox="0 0 512 512" style="height: 50px; width: 50px;">
+            <svg version="1.1" viewBox="0 0 512 512" style="height: 50px; width: 50px; fill: var(--elara-font-color);">
                 <path d="M256,8.144c-141.152,0-256,114.848-256,256C0,369.456,66.016,465.36,164.224,502.768c4.096,1.632,8.704,1.376,12.672-0.544c3.968-1.952,6.976-5.408,8.288-9.632l48-153.28c2.272-7.264-0.864-15.136-7.52-18.784C204.896,309.072,192,287.472,192,264.144c0-35.296,28.704-64,64-64s64,28.704,64,64c0,23.328-12.896,44.928-33.664,56.416c-6.656,3.648-9.824,11.52-7.52,18.784l48,153.28c1.312,4.224,4.32,7.68,8.288,9.632c2.208,1.056,4.608,1.6,6.976,1.6c1.92,0,3.872-0.352,5.696-1.056C446.016,465.36,512,369.456,512,264.144C512,122.992,397.152,8.144,256,8.144z"/>
             </svg>
             </a>
@@ -517,4 +517,4 @@ class r{constructor(e,t,n){this.__parts=[],this.template=e,this.processor=t,this
 			<div id="content" class="content"></div>
 			<ui-menu id="menu" .items=${this.links} .route=${this.route}></ui-menu>
 		`}_showMenu(){return L(this,void 0,void 0,(function*(){if(this._menu.shown)return void(yield this._hideMenu());if(this._menuFade)return;this._content.classList.contains("hidden")||this._content.classList.add("hidden"),!1===this._menu.shown&&(this._menu.shown=!0);const e=r(300,!0);this._menuFade=this._menu.animate(e.effect,e.options),yield this._menuFade.finished,this._menuFade=null}))}_hideMenu(){return L(this,void 0,void 0,(function*(){if(this._menuFade)return;const e=r(300,!1);this._menuFade=this._menu.animate(e.effect,e.options),yield this._menuFade.finished,this._content.classList.remove("hidden"),this._menu.shown=!1,this._menuFade=null}))}}M.is="elara-app",customElements.define(M.is,M)}]);
-//# sourceMappingURL=main.fcfe29a3.js.map
+//# sourceMappingURL=main.536bdffd.js.map
